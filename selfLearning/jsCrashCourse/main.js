0d8b9daca52f9afadd8c5a0eb25ec96f68694471
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-02-27 13:29:50 +0900

    finished jsCrashCourse

--- selfLearning/jsCrashCourse/main.js
+++ selfLearning/jsCrashCourse/main.js
@@ -35,4 +35,73 @@ const ul = document.querySelector('.items'); // First Occurance
 
 // ul.firstElementChild.textContent = "Kishan";
 // ul.children[1].innerText = 'Kishan';
-ul.lastElementChild.innerHTML = ''
+// ul.lastElementChild.innerHTML = '<h1>HELLO</h1>'
+
+// const btn = document.querySelector('.btn');
+// btn.style.background = 'green'
+
+// let count = 0;
+// //--------------Events simple
+// const btn = document.querySelector('.btn');
+// btn.addEventListener('click', function(e){
+// 	e.preventDefault(); //To remove submitting of form
+// 	count+=1
+// 	console.log(`clicked ${count}`);
+// 	console.log(e.target.id); //Gives the targeted element and its properties can be fetched
+// 	console.log(e);
+// 	e.target.style.background = 'red';
+// 	console.log(`The background of ${e.target} is ${e.target.style.background}`)
+// 	btn.style.background = 'blue';
+// 	console.log(`The background of ${btn} is ${btn.style.background}`)
+// })
+
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('mouseover', function(e){
+// 	e.preventDefault();
+// 	document.querySelector('.items').lastElementChild.style.backgroundColor = 'red';
+// })
+
+// const user = []
+// const myForm = document.querySelector('#my-form');
+// const nameInput = document.querySelector('#name');
+// const emailInput = document.querySelector('#email');
+// const msg = document.querySelector('.msg');
+// const userList = document.querySelector('#users');
+
+// myForm.addEventListener('submit', onSubmit);
+
+// function onSubmit(e){
+// 	e.preventDefault();
+// 	if(nameInput.value === '' || emailInput.value === ''){
+// 		msg.classList.add('error')
+// 		msg.innerHTML = "Please enter all fields";
+
+// 		setTimeout(() => {
+// 			msg.classList.remove('error');
+// 			msg.innerHTML = ''
+// 		}, 1000);
+// 	}else{
+// 		console.log('Success');
+// 		user.push({
+// 			name: nameInput.value,
+// 			email: emailInput.value
+// 		});
+
+// 		renderUserList();
+
+// 		//clear fields
+// 		nameInput.value = '';
+// 		emailInput.value = '';
+// 	}
+// }
+
+// function renderUserList(){
+// 	userList.innerHTML = '';
+// 	user.forEach(u =>{
+// 		const li = document.createElement('li');
+// 		li.appendChild(document.createTextNode(`${u.name} : ${u.email}`));
+// 		userList.appendChild(li);
+// 	}
+// 	)
+// };
