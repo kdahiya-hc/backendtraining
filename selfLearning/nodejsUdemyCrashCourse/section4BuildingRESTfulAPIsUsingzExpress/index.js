cba24ab1c4c0f38662d6f2ff04240aeb93ace30e
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-04 14:23:14 +0900

    47 Route parameters

--- selfLearning/nodejsUdemyCrashCourse/section4BuildingRESTfulAPIsUsingzExpress/index.js
+++ selfLearning/nodejsUdemyCrashCourse/section4BuildingRESTfulAPIsUsingzExpress/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const PORT = 5005;
+const PORT = process.env.PORT || 5005;
 
 const app = express();
 
@@ -9,7 +9,19 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/courses', (req, res) => {
-	res.send(["Java","Express","Node"]);
+	res.send(["Java", "Express", "Node", "Python"]);
+});
+
+app.get('/api/courses', (req, res) => {
+	res.send(`req.query: ${JSON.stringify(req.query)}`);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+	res.send(`req.params.id: ${req.params.id}`);
+});
+
+app.get('/api/courses/:year/:month', (req, res) => {
+	res.send(`req.params: ${req.params}`);
 });
 
 app.listen(PORT, () => {
