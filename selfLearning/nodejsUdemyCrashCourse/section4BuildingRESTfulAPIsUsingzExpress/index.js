7137fadf0eca560cd3cd40c7ae851837d9fcffe0
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-04 15:47:08 +0900

    Made repeated validation code a function

--- selfLearning/nodejsUdemyCrashCourse/section4BuildingRESTfulAPIsUsingzExpress/index.js
+++ selfLearning/nodejsUdemyCrashCourse/section4BuildingRESTfulAPIsUsingzExpress/index.js
@@ -15,6 +15,15 @@ const courses = [
 	{id: 4, name: "Python"},
 ];
 
+function validateCourse(req, res){
+	const schema = Joi.object({
+		name: Joi.string().min(4).required(),
+	});
+	const {error, value} = schema.validate(req.body);
+	if (error){ return res.status(400).send(error.details[0].message); }
+	return value;
+}
+
 app.get('/api/courses', (req, res) => {
 	res.send(courses);
 });
@@ -26,12 +35,7 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-	const schema = Joi.object({
-		name: Joi.string().min(4).required(),
-	});
-
-	const {error, value} = schema.validate(req.body);
-	if (error){ return res.status(400).send(error.details[0].message); }
+	const value = validateCourse(req, res);
 	const newCourse = {
 		id: courses.length + 1,
 		name: value.name,
@@ -46,13 +50,8 @@ app.put('/api/courses/:id', (req, res) => {
 	const course = courses.find(c => c.id === parseInt(req.params.id));
 	if ( !course ) { return res.status(404).send('Course not found!!!'); }
 
-	const schema = Joi.object({
-		name: Joi.string().min(4).required(),
-	});
-
 	// Validate name (repeated)
-	const {error, value} = schema.validate(req.body);
-	if (error){ return res.status(400).send(error.details[0].message); }
+	const value = validateCourse(req, res)
 
 	// Update data
 	course.name = value.name;
