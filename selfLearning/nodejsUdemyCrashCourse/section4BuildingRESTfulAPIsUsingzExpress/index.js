eb5f4c1aafe163ecb2a4b998dd550ddd8a51084d
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-04 15:03:21 +0900

    49 Handling http POST Request

--- selfLearning/nodejsUdemyCrashCourse/section4BuildingRESTfulAPIsUsingzExpress/index.js
+++ selfLearning/nodejsUdemyCrashCourse/section4BuildingRESTfulAPIsUsingzExpress/index.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 5005;
 
 const app = express();
 
+//Middleware in request processing handler
+app.use(express.json());
+
 const courses = [
 	{id: 1, name: "Java"},
 	{id: 2, name: "Node"},
@@ -19,6 +22,24 @@ app.get('/api/courses/:id', (req, res) => {
 	const course = courses.find(c => c.id === parseInt(req.params.id));
 	if (!course){res.status(404).send('The course with the given ID is not found!!!')};
 	res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+	// const newCourse = req.body;
+	// if (!newCourse.id || !newCourse.name){
+	// 	return res.status(404).send('Course Name and id are both required');
+	// }
+	if (!req.body.name){
+		return res.status(404).send('Course Name is required');
+	}
+
+	const newCourse = {
+		id: courses.length + 1,
+		name: req.body.name
+	};
+
+	courses.push(newCourse);
+	res.status(201).json(newCourse);
 });
 
 app.listen(PORT, () => {
