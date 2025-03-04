9f2b3f2471d52f630a4617e85a7655350b128319
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-04 15:42:36 +0900

    51, 52 Input validation, Handling PUT request

--- selfLearning/nodejsUdemyCrashCourse/section4BuildingRESTfulAPIsUsingzExpress/index.js
+++ selfLearning/nodejsUdemyCrashCourse/section4BuildingRESTfulAPIsUsingzExpress/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Joi = require('joi');
 
 const PORT = process.env.PORT || 5005;
 
@@ -20,28 +21,44 @@ app.get('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
 	const course = courses.find(c => c.id === parseInt(req.params.id));
-	if (!course){res.status(404).send('The course with the given ID is not found!!!')};
+	if (!course){res.status(404).send('The course with the given ID is not found!!!');}
 	res.send(course);
 });
 
 app.post('/api/courses', (req, res) => {
-	// const newCourse = req.body;
-	// if (!newCourse.id || !newCourse.name){
-	// 	return res.status(404).send('Course Name and id are both required');
-	// }
-	if (!req.body.name){
-		return res.status(404).send('Course Name is required');
-	}
+	const schema = Joi.object({
+		name: Joi.string().min(4).required(),
+	});
 
+	const {error, value} = schema.validate(req.body);
+	if (error){ return res.status(400).send(error.details[0].message); }
 	const newCourse = {
 		id: courses.length + 1,
-		name: req.body.name
+		name: value.name,
 	};
 
 	courses.push(newCourse);
 	res.status(201).json(newCourse);
 });
 
+app.put('/api/courses/:id', (req, res) => {
+	// Look up
+	const course = courses.find(c => c.id === parseInt(req.params.id));
+	if ( !course ) { return res.status(404).send('Course not found!!!'); }
+
+	const schema = Joi.object({
+		name: Joi.string().min(4).required(),
+	});
+
+	// Validate name (repeated)
+	const {error, value} = schema.validate(req.body);
+	if (error){ return res.status(400).send(error.details[0].message); }
+
+	// Update data
+	course.name = value.name;
+	res.status(200).send(course);
+});
+
 app.listen(PORT, () => {
-	console.log(`Listening on http://localhost:${PORT}`)
-})
+	console.log(`Listening on http://localhost:${PORT}`);
+});
