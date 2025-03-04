47f7b1f44af64940da872bf8c942bedfec4d7711
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-04 17:52:55 +0900

    temp push at day end

--- selfLearning/nodejsUdemyCrashCourse/projectVidly/index.js
+++ selfLearning/nodejsUdemyCrashCourse/projectVidly/index.js
@@ -6,6 +6,8 @@ const Joi = require('joi');
 // Initialize express??
 const app = express();
 
+// Initialize
+
 // Middleware
 app.use(express.json());
 
@@ -13,10 +15,42 @@ app.use(express.json());
 const PORT = process.env.PORT || 5005;
 
 // Static object which shall be for genres
+const genres = [
+	{'name':'action'},
+	{'name':'horror'},
+	{'name':'comedy'},
+	{'name':'romance'},
+	{'name':'drama'},
+	{'name':'thriller'},
+	{'name':'western'},
+	{'name':'sci-fi'},
+];
 
+// Function to validate genre name
+function validateGenreName(req, res){
+	const schema = Joi.object({
+		name: Joi.string.min(4).required(),
+	});
+	const {error, value} = schema.validate(req.body);
+	if (error){ return res.status(400).send(error.details[0].message); }
+	return value;
+};
+
+// Home or default route
 app.get('/', (req, res) => {
 	res.status(200).send('<h1>Home Page<h1>');
 });
+
+// GET all genre
+app.get('./api/genres', (req, res) => {
+	res.status(200).send(genres);
+})
+
+// POST create a genre
+
+// PUT update a genre
+
+// DELETE delete a genre
 
 app.listen(PORT, () => {
 	console.log(`Listening on http://localhost:${PORT}`);
