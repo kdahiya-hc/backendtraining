55933979670b009ade2ee2decb27f71a316296ca
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-05 09:03:00 +0900

    task 1 checkpoint

--- selfLearning/nodejsUdemyCrashCourse/projectVidly/index.js
+++ selfLearning/nodejsUdemyCrashCourse/projectVidly/index.js
@@ -16,20 +16,20 @@ const PORT = process.env.PORT || 5005;
 
 // Static object which shall be for genres
 const genres = [
-	{ id:1, 'name':'action'},
-	{ id:2, 'name':'horror'},
-	{ id:3, 'name':'comedy'},
-	{ id:4, 'name':'romance'},
-	{ id:5, 'name':'drama'},
-	{ id:6, 'name':'thriller'},
-	{ id:7, 'name':'western'},
-	{ id:8, 'name':'sci-fi'},
+	{ id:1, 'typeOfGenre':'action'},
+	{ id:2, 'typeOfGenre':'horror'},
+	{ id:3, 'typeOfGenre':'comedy'},
+	{ id:4, 'typeOfGenre':'romance'},
+	{ id:5, 'typeOfGenre':'drama'},
+	{ id:6, 'typeOfGenre':'thriller'},
+	{ id:7, 'typeOfGenre':'western'},
+	{ id:8, 'typeOfGenre':'sci-fi'},
 ];
 
-// Function to validate genre name
-function validateGenreName(req, res){
+// Function to validate genre typeOfGenre
+function validateGenreType(req, res){
 	const schema = Joi.object({
-		name: Joi.string.min(4).required(),
+		typeOfGenre: Joi.string.min(4).required(),
 	});
 	const {error, value} = schema.validate(req.body);
 	if (error){ return res.status(400).send(error.details[0].message); }
@@ -46,11 +46,50 @@ app.get('/api/genres', (req, res) => {
 	res.status(200).send(genres);
 })
 
+// GET genre by id
+app.get('/api/genres/:id', (req, res) => {
+	// Use find() an array function which also calls a fucntion that returns
+	const genre = genres.find( x => x.id === parseInt(req.params.id));
+	if (!genre) { return res.status(404).send('No genre found with the provided ID'); }
+	res.status(200).send(genre);
+});
+
+// GET genre by typeOfGenre
+app.get('/api/genres/:typeOfGenre', (req, res) => {
+	// Use find() an array function which also calls a fucntion that returns
+	const genre = genres.find( x => x.typeOfGenre === req.params.typeOfGenre);
+	if (!genre) { return res.status(404).send('No genre found with the provided type'); }
+	res.status(200).send(genre);
+});
+
 // POST create a genre
+app.post('/api/genres', (req, res) => {
+	// Validate
+
+	// Create
+
+	// Reply
+});
 
 // PUT update a genre
+app.put('/api/genres/:id', (req, res) => {
+	// Find
+
+	// Validate
+
+	// Update
+
+	// Reply
+});
 
 // DELETE delete a genre
+app.delete('/api/genres', (req, res) => {
+	// Find
+
+	// Delete
+
+	// Reply
+});
 
 app.listen(PORT, () => {
 	console.log(`Listening on http://localhost:${PORT}`);
