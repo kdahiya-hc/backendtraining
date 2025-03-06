90225cbea0077e21dba60a37157f5839ec2d33ee
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-06 10:25:34 +0900

    72 Callbacks

---
+++ selfLearning/nodejsUdemyCrashCourse/section6AsyncSyncJS/index.js
@@ -0,0 +1,35 @@
+getUser(1, (user) => {
+	console.log(user);
+	getRepos(user.username, (repositories) => {
+		console.log(repositories);
+	});
+});
+
+function getUser(id, getUserCallback){
+	setTimeout(function(){
+		console.log('Getting user data');
+		getUserCallback({ id: id, gitHubUsername: 'kdahiya-hc'});
+	}, 2000);
+}
+
+function getRepos(username, getReposCallback){
+	setTimeout(() => {
+		console.log('Getting repository')
+		getReposCallback(['repo1', 'repo2']);
+	},2000);
+}
+
+// // callbacks
+// getUsers((users) => {
+// 	console.log(users);
+// });
+
+// function getUsers(callback){
+// 	setTimeout(() => {
+// 		console.log('Logging');
+// 		callback([
+// 			{ id: 1, username: 'Kishan' },
+// 			{ id: 2, username: 'Jayshah'},
+// 		])
+// 	}, 1000);
+// }
