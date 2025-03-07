2964497f557fa3640dc4a977d5e9e51eac25f7fd
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-07 15:35:46 +0900

    section7 exercise 3 done

---
+++ selfLearning/nodejsUdemyCrashCourse/exercises/section7/exercise3/index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const dbURI = 'mongodb://admin:june1999@localhost:27017/selfLearning?authSource=admin';
+
+mongoose.connect(dbURI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log('Could not connect:',err.message));
+
+// Schema for Object
+const courseSchema = mongoose.Schema({
+	name: String,
+	author: String,
+	tags: [String],
+	price: Number,
+	date: {type: Date, default: Date.now },
+	isPublished: Boolean,
+})
+// Model or Class
+const Course = mongoose.model('Course',courseSchema);
+
+// Document in Collection
+async function getCourses() {
+	const courses = await Course
+	.find({isPublished: true})
+	.or([{ price: {$gte: 15}}, {name: /.*by.*/i}])
+	console.log(courses);
+}
+
+getCourses();
