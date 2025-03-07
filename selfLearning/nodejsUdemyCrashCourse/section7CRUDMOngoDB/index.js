455a2def7245a954df3a9c893e123ca59037b196
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-07 12:50:41 +0900

    connected MongoDB

--- selfLearning/nodejsUdemyCrashCourse/section7CRUDMOngoDB/index.js
+++ selfLearning/nodejsUdemyCrashCourse/section7CRUDMOngoDB/index.js
@@ -1,1 +1,9 @@
+require('dotenv').config(); // ✅ Load environment variables at the top
+const config = require('config');
+const mongoose = require('mongoose');
 
+const dbUri = `mongodb://${config.get('db.user')}:${config.get('db.password')}@${config.get('db.host')}:${config.get('db.port')}/${config.get('db.database')}?authSource=admin`;
+
+mongoose.connect(dbUri)
+	.then(() => console.log('Connected to MongoDB'))
+	.catch(err => console.log('Could not connect:',err.message))
