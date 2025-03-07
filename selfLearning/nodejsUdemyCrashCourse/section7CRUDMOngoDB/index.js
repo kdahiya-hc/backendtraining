f478f4151076cec53b45dd435be09b683866cb9a
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-07 13:29:23 +0900

    89 Querying Document of Mongo DB

--- selfLearning/nodejsUdemyCrashCourse/section7CRUDMOngoDB/index.js
+++ selfLearning/nodejsUdemyCrashCourse/section7CRUDMOngoDB/index.js
@@ -6,4 +6,44 @@ const dbUri = `mongodb://${config.get('db.user')}:${config.get('db.password')}@$
 
 mongoose.connect(dbUri)
 	.then(() => console.log('Connected to MongoDB'))
-	.catch(err => console.log('Could not connect:',err.message))
+	.catch(err => console.log('Could not connect:',err.message));
+
+// Define schema
+const courseSchema = new mongoose.Schema({
+	name: String,
+	author: String,
+	tags: [String ],
+	date: {type: Date, default: Date.now },
+	isPublished: Boolean,
+});
+
+// Model create , populate with documents
+// Class as first letter Caps Pascal case
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+	// Object , camel case
+	const course = new Course({
+		name: 'Angular Course',
+		author: 'kdahiya-hc',
+		tags: ['angula', 'frontend'],
+		isPublished: true,
+	});
+
+	// document is ready next save to collection
+	const result = await course.save();
+	console.log(result);
+}
+
+async function getCourse() {
+	const courses = await Course
+	.find({ author: 'kdahiya-hc'})
+	.limit(10)
+	.sort({ name: 1})
+	.select({name: 1, tags: 1})
+	console.log(courses);
+}
+
+// createCourse();
+
+getCourse();
