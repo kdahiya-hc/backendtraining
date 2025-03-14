d092a0246bf14bbf47325c300e85a6765bfe2e68
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-14 13:42:26 +0900

    protected routes, removed methods that require role admin

--- selfLearning/nodejsUdemyCrashCourse/exercises/section10/projectVidly/routes/users.js
+++ selfLearning/nodejsUdemyCrashCourse/exercises/section10/projectVidly/routes/users.js
@@ -1,31 +1,32 @@
 const _ = require('lodash');const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
+// const {auth} = require('../middlewares/auth.js')
 const { User, validateUser } = require('../models/user.js');
 
-// GET all users
-router.get('/', async (req, res) => {
-	try {
-		const users = await User.find().sort('name');
-		res.status(200).json(users);
-	} catch (err) {
-		res.status(500).json({ message: 'Error retrieving the users', error: err.message });
-	}
-});
+// // GET all users
+// router.get('/', async (req, res) => {
+// 	try {
+// 		const users = await User.find().sort('name');
+// 		res.status(200).json(users);
+// 	} catch (err) {
+// 		res.status(500).json({ message: 'Error retrieving the users', error: err.message });
+// 	}
+// });
 
-// GET user by email
-router.get('/:email', async (req, res) => {
-	try {
-		const user = await User.findOne({ email: req.params.email });
-		if (!user) {
-			return res.status(404).json({ message: 'No user found with provided email.' });
-		}
+// // GET user by email
+// router.get('/:email', async (req, res) => {
+// 	try {
+// 		const user = await User.findOne({ email: req.params.email });
+// 		if (!user) {
+// 			return res.status(404).json({ message: 'No user found with provided email.' });
+// 		}
 
-		res.status(200).json({ user: _.pick(user, ['_id', 'name', 'email']) });
-	} catch (err) {
-		res.status(500).json({ message: 'Error retrieving the user', error: err.message });
-	}
-});
+// 		res.status(200).json({ user: _.pick(user, ['_id', 'name', 'email']) });
+// 	} catch (err) {
+// 		res.status(500).json({ message: 'Error retrieving the user', error: err.message });
+// 	}
+// });
 
 // POST create a user
 router.post('/', async (req, res) => {
@@ -58,48 +59,48 @@ router.post('/', async (req, res) => {
 	}
 });
 
-// PUT update user by email
-router.put('/:email', async (req, res) => {
-	// Validate the request body
-	const { error, value } = validateUser(req.body);
-	if (error) return res.status(400).send(error.details[0].message);
+// // PUT update user by email
+// router.put('/:email', async (req, res) => {
+// 	// Validate the request body
+// 	const { error, value } = validateUser(req.body);
+// 	if (error) return res.status(400).send(error.details[0].message);
 
-	const salt = await bcrypt.genSalt(5);
-	const hashedPassword = await bcrypt.hash(value.password, salt);
+// 	const salt = await bcrypt.genSalt(5);
+// 	const hashedPassword = await bcrypt.hash(value.password, salt);
 
-	try {
-		const user = await User.findOneAndUpdate(
-			{ email: req.params.email },
-			{
-				$set: {
-					name: value.name,
-					password: hashedPassword,
-				},
-			},
-			{ new: true }
-		);
-		if (!user) {
-			return res.status(404).json({ message: 'No user found with provided email.' });
-		}
+// 	try {
+// 		const user = await User.findOneAndUpdate(
+// 			{ email: req.params.email },
+// 			{
+// 				$set: {
+// 					name: value.name,
+// 					password: hashedPassword,
+// 				},
+// 			},
+// 			{ new: true }
+// 		);
+// 		if (!user) {
+// 			return res.status(404).json({ message: 'No user found with provided email.' });
+// 		}
 
-		res.status(200).json({ message: 'User has been updated successfully.', user: _.pick(user, ['_id', 'name', 'email']) });
-	} catch (err) {
-		res.status(500).json({ message: 'Error updating the user', error: err.message });
-	}
-});
+// 		res.status(200).json({ message: 'User has been updated successfully.', user: _.pick(user, ['_id', 'name', 'email']) });
+// 	} catch (err) {
+// 		res.status(500).json({ message: 'Error updating the user', error: err.message });
+// 	}
+// });
 
-// DELETE user by email
-router.delete('/:email', async (req, res) => {
-	try {
-		const user = await User.findOneAndDelete({ email: req.params.email });
-		if (!user) {
-			return res.status(404).json({ message: 'No user found with provided email.' });
-		}
+// // DELETE user by email
+// router.delete('/:email', async (req, res) => {
+// 	try {
+// 		const user = await User.findOneAndDelete({ email: req.params.email });
+// 		if (!user) {
+// 			return res.status(404).json({ message: 'No user found with provided email.' });
+// 		}
 
-		res.status(200).json({ message: 'User has been deleted successfully.', user:  _.pick(user, ['_id', 'name', 'email']) });
-	} catch (err) {
-		res.status(500).json({ message: 'Error deleting the user', error: err.message });
-	}
-});
+// 		res.status(200).json({ message: 'User has been deleted successfully.', user:  _.pick(user, ['_id', 'name', 'email']) });
+// 	} catch (err) {
+// 		res.status(500).json({ message: 'Error deleting the user', error: err.message });
+// 	}
+// });
 
 module.exports = router;
