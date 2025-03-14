ae64cb187d8eb6b2275bd8680714bb4662a18ca7
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-14 10:32:22 +0900

    bcrypt: missed lines added

--- selfLearning/nodejsUdemyCrashCourse/section10/hash.js
+++ selfLearning/nodejsUdemyCrashCourse/section10/hash.js
@@ -1,11 +1,24 @@
 const bcrypt = require('bcrypt');
 
-const password = 'june1999'
-
 async function run() {
-	console.log(`Salt: ${salt}`);
-	console.log(`Password: ${password}`);
-	console.log(`Hashed Password: ${hashedPassword}`);
+    const password = 'june1999';
+
+    // Generate salt and hash password
+    const salt = await bcrypt.genSalt(5);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    console.log(`Salt: ${salt}`);
+    console.log(`Password: ${password}`);
+    console.log(`Hashed Password: ${hashedPassword}`);
+
+    // Call password match function
+    await matchP(password, hashedPassword);
 }
 
+async function matchP(password, hashedPassword) {
+    const result = await bcrypt.compare('lol', hashedPassword);
+    console.log(`Password Match: ${result}`);
+}
+
+// Run the function
 run();
