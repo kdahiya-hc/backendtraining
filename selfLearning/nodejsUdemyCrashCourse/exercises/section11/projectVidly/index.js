9a6afad91d5539ff2e0d6193ffccc48d6a714a70
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-18 10:59:31 +0900

    Exit when unhandled or uncaught error exists, best use process managers

--- selfLearning/nodejsUdemyCrashCourse/exercises/section11/projectVidly/index.js
+++ selfLearning/nodejsUdemyCrashCourse/exercises/section11/projectVidly/index.js
@@ -22,10 +22,12 @@ const app = express();
 
 process.on('uncaughtException', (err) => {
 	logger.error(err.message, err);
+	process.exit(1);
 });
 
 process.on('unhandledRejection', (err) => {
 	logger.error(err.message, err);
+	process.exit(1);
 });
 
 const PORT = process.env.PORT || 5005;
