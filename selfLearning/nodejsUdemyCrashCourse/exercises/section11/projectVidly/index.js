df4b1d39a1aa0acc89d19072280a5ec37a505cc4
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-18 17:20:59 +0900

    fixed request body undefined

--- selfLearning/nodejsUdemyCrashCourse/exercises/section11/projectVidly/index.js
+++ selfLearning/nodejsUdemyCrashCourse/exercises/section11/projectVidly/index.js
@@ -7,14 +7,6 @@ require('./startup/db.js')();
 require('./startup/config.js')();
 require('./startup/validation.js')();
 
-// Use express middleware for both JSON and URL-encoded data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// View engine and views folder setup (if you're using Pug)
-app.set('view engine', 'pug');
-app.set('views', './views');
-
 // Start the server
 const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => {
--- selfLearning/nodejsUdemyCrashCourse/exercises/section11/projectVidly/startup/routes.js
+++ selfLearning/nodejsUdemyCrashCourse/exercises/section11/projectVidly/startup/routes.js
@@ -1,4 +1,5 @@
 // Import routes
+const express = require('express');
 const movies = require('../routes/movies.js')
 const genres = require('../routes/genres.js');
 const users = require('../routes/users.js');
@@ -9,6 +10,14 @@ const home = require('../routes/home.js');
 const { error } = require('../middlewares/error.js');
 
 const routes = (app)=> {
+	// Use express middleware for both JSON and URL-encoded data
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: true }));
+
+	// View engine and views folder setup (if you're using Pug)
+	app.set('view engine', 'pug');
+	app.set('views', './views');
+
 	// Register all the routes
 	app.use('/api/genres', genres);
 	app.use('/api/users', users);
