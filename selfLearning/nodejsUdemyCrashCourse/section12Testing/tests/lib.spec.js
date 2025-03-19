be95b20224b0865df4333fb8c3b9addb071dfb74
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-19 09:47:00 +0900

    grouping test

--- selfLearning/nodejsUdemyCrashCourse/section12Testing/tests/lib.spec.js
+++ selfLearning/nodejsUdemyCrashCourse/section12Testing/tests/lib.spec.js
@@ -1,16 +1,18 @@
 const lib = require('../lib');
 
-test('absolute - should return a positive number: when input is positive', () => {
-	const result = lib.absolute(1);
-	expect(result).toBe(1);
-});
+describe('absolute', () => {
+	it('absolute - should return a positive number: when input is positive', () => {
+		const result = lib.absolute(1);
+		expect(result).toBe(1);
+	});
 
-test('absolute - should return a positive number: when input is negative', () => {
-	const result = lib.absolute(-1);
-	expect(result).toBe(1);
-});
+	it('absolute - should return a positive number: when input is negative', () => {
+		const result = lib.absolute(-1);
+		expect(result).toBe(1);
+	});
 
-test('absolute - should return a zero number: when input is zero', () => {
-	const result = lib.absolute(0);
-	expect(result).toBe(0);
-});
+	it('absolute - should return a zero number: when input is zero', () => {
+		const result = lib.absolute(0);
+		expect(result).toBe(0);
+	});
+})
