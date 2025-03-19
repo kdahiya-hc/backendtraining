89018e026cf6e7082827da88150b974284d16de4
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-19 09:46:39 +0900

    multiple tests

--- selfLearning/nodejsUdemyCrashCourse/section12Testing/tests/lib.spec.js
+++ selfLearning/nodejsUdemyCrashCourse/section12Testing/tests/lib.spec.js
@@ -1,3 +1,16 @@
-test('First Test', () => {
-	throw new Error('First Error!')
-})
+const lib = require('../lib');
+
+test('absolute - should return a positive number: when input is positive', () => {
+	const result = lib.absolute(1);
+	expect(result).toBe(1);
+});
+
+test('absolute - should return a positive number: when input is negative', () => {
+	const result = lib.absolute(-1);
+	expect(result).toBe(1);
+});
+
+test('absolute - should return a zero number: when input is zero', () => {
+	const result = lib.absolute(0);
+	expect(result).toBe(0);
+});
