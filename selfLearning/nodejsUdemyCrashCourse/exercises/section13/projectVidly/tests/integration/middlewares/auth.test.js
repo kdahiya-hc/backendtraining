950c43a5b392795b5ed22db8d11035f586ebdf0b
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-21 17:07:06 +0900

    check token valid and invalid, clear genres

--- selfLearning/nodejsUdemyCrashCourse/exercises/section13/projectVidly/tests/integration/middlewares/auth.test.js
+++ selfLearning/nodejsUdemyCrashCourse/exercises/section13/projectVidly/tests/integration/middlewares/auth.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const auth = require('../../../middlewares/auth');
+const {Genre} = require('../../../models/genre');
 const {User} = require('../../../models/user');
 
 describe('auth middleware', () => {
@@ -10,6 +11,7 @@ describe('auth middleware', () => {
 	});
  	afterEach( async() => {
 		// await server.close();
+		await Genre.deleteMany({});
 	});
 
 	const exec = () => {
@@ -27,5 +29,16 @@ describe('auth middleware', () => {
 		token = '';
 		const res = await exec();
 		expect(res.status).toBe(401);
+	});
+
+	it('should return 400 if token is invalid', async() => {
+		token = 'a';
+		const res = await exec();
+		expect(res.status).toBe(400);
+	});
+
+	it('should return 201 if token is valid', async() => {
+		const res = await exec();
+		expect(res.status).toBe(201);
 	});
 });
