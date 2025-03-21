79af8f0e2ad3bdaf7831662123515af673a5be81
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-21 10:37:41 +0900

    185 testing the get route

--- selfLearning/nodejsUdemyCrashCourse/exercises/section13/projectVidly/tests/integration/genres.test.js
+++ selfLearning/nodejsUdemyCrashCourse/exercises/section13/projectVidly/tests/integration/genres.test.js
@@ -1,14 +1,27 @@
 const request = require('supertest');
+const {Genre} = require('../../models/genre');
 let server;
 
 describe('/api/genres', () => {
-	beforeEach( () => { server = require('../../index'); });
-	afterEach( () => { server.close(); });
+	beforeEach( () => {
+		server = require('../../index');
+	});
+	afterEach( async() => {
+		await server.close();
+		await Genre.deleteMany({});
+	});
 
 	describe('GET /', () => {
 		it('should return all genres', async() => {
+			await Genre.collection.insertMany([
+				{ typeOfGenre: 'Horror'},
+				{ typeOfGenre: 'Romance'},
+			]);
 			const res = await request(server).get('/api/genres');
 			expect(res.status).toBe(200);
+			expect(res.body.length).toBe(2);
+			expect(res.body.some((g)=> { return g.typeOfGenre === 'Horror'})).toBeTruthy();
+			expect(res.body.some((g)=> { return g.typeOfGenre === 'Romance'})).toBeTruthy();
 		});
 	});
 });
