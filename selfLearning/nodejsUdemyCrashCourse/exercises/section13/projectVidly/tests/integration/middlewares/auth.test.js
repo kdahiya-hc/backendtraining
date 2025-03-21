10e5594274e5ddfc96578defccc37d93490da62c
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-21 16:04:33 +0900

    server is causing issue

---
+++ selfLearning/nodejsUdemyCrashCourse/exercises/section13/projectVidly/tests/integration/middlewares/auth.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const auth = require('../../../middlewares/auth');
+const {User} = require('../../../models/user');
+
+describe('auth middleware', () => {
+	let token;
+
+	beforeEach( () => {
+		server = require('../../../index')
+	});
+ 	afterEach( async() => { await server.close(); });
+
+	const exec = () => {
+		return request(server)
+			.post('/api/genres')
+			.set('x-auth-token', token)
+			.send({typeOfGenre: 'Testing'})
+	}
+
+	beforeEach( () => {
+		token = new User().generateAuthToken();
+	});
+
+	it('should return 401 if no token is provided', async() => {
+		token = '';
+		const res = await exec();
+		expect(res.status).toBe(401);
+	});
+});
