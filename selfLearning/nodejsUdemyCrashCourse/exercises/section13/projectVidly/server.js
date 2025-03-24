641b4d7167f87e24b1c69812ab5c92d09aca5927
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-24 11:06:44 +0900

    fixed the error in project of section13

--- selfLearning/nodejsUdemyCrashCourse/exercises/section13/projectVidly/server.js
+++ selfLearning/nodejsUdemyCrashCourse/exercises/section13/projectVidly/server.js
@@ -1,5 +1,5 @@
 // server.js
-const app = require('./server');  // Import the app created in index.js
+const app = require('./app.js');
 const logger = require('./startup/logger.js');
 
 const PORT = process.env.PORT || 5005;
@@ -7,4 +7,4 @@ const server = app.listen(PORT, () => {
     logger.info(`Server is listening on http://localhost:${PORT}`);
 });
 
-module.exports = server;  // Export the server for testing if needed
+module.exports = server;
