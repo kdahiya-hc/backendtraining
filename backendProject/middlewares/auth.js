296433ab12163bf16e6a144981abfd681f64d2e1
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-25 16:00:41 +0900

    add auth middleware which checks header for token if exists add to req.user

--- backendProject/middlewares/auth.js
+++ backendProject/middlewares/auth.js
@@ -1,6 +1,20 @@
+require('dotenv').config();
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
 function auth(req, res, next) {
-	console.log('Authenticating...');
-	next();
+	const token = req.header('x-auth-token');
+	if (!token) {
+		return res.status(401).json({message: 'Access denied!'});
+	}
+
+	try {
+		const decodedPayload = jwt.verify(token, config.get('jwtPrivateKey') );
+		req.user = decodedPayload;
+		next();
+	} catch(err) {
+		res.status(400).json({ message: 'Error', error: err.message });
+	}
 }
 
- module.exports = auth;
+module.exports = auth;
