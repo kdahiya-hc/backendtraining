a4746e4270eabebd3b9cfba86f5372a5986202d9
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-26 09:47:30 +0900

    added morgan to see which routes are hit on console

--- backendProject/server.js
+++ backendProject/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const config = require('config');
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const dbConfig = config.get('db');
 const database = process.env[dbConfig.database]
@@ -16,9 +17,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 app.use('/', require('./routes/home'));
 app.use('/api/users', require('./routes/user'));
+app.use('/api/auth', require('./routes/auth'));
 
 const PORT = config.get('port');
 
