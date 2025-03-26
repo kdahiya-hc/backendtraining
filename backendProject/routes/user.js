6218e63ea081a4bbfc36d6a0872c3dcc749a8eb4
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-26 09:47:12 +0900

    updated user routes

--- backendProject/routes/user.js
+++ backendProject/routes/user.js
@@ -1,92 +1,87 @@
 const _ = require('lodash');
-const {User, validate } = require('../models/User');
+const { User, validate } = require('../models/User');
 const auth = require('../middlewares/auth');
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
 
-// GET user detail with email
-router.get('/:email', async (req, res) => {
-	try{
-		const user = await User.findOne({ email: req.params.email});
+// Get all users details with pagination
+router.get('/', auth, async (req, res) => {
+	try {
+		console.log('In get all with pagination')
+		const page = parseInt(req.query.page) || 1;
+		const limit = parseInt(req.query.limit) || 5;
+		const skip = (page - 1) * limit;
 
-		if(!user){
-			return res.status(404).json({ message: 'No user found'});
-		}
+		const totalUsers = await User.countDocuments();
+		const users = await User.find().skip(skip).limit(limit);
 
-		const userData =  _.pick(user, ['email', 'name']);
+		if (users.length === 0) return res.status(200).json({ message: 'No Users found', users : [] });
+
+		const usersData = users.map(user => _.pick(user, ['email', 'name']))
+		return res.status(200).json({ message: 'Users found', users : usersData, totalUsers });
+	} catch(err) {
+		console.log(err.message);
+		return res.status(500).json({ message: 'Something happened', error: err.message});
+	}
+});
+
+// Get logged in user details
+router.get('/me', auth, async (req, res) => {
+	try {
+		console.log('In view own page')
+		const user = await User.findById({ _id : req.user._id });
+
+		if (!user) return res.status(404).json({ message: 'No user found' });
+
+		return res.status(200).json({ message: 'User found', user: _.pick(user, ['email', 'name']) });
+	} catch(err){
+		console.log(err.message);
+		return res.status(500).json({ message: 'Something happened', error: err.message});
+	}
+})
+
+// Update logged in user details
+router.put('/me', auth, async (req, res) => {
+	try {
+		console.log('In update')
+		const { error, value } = validate(req.body);
+		if (error) return res.status(400).send(error.details[0].message);
+
+		const user = await User.findOneAndUpdate(
+			{ _id : req.user._id },
+			{
+				$set: {
+					name:value.name,
+					address:value.address,
+					dob: value.dob,
+				},
+			},
+			{ new: true}
+		);
+
+		if (!user) return res.status(404).json({ message: 'No user found' });
+
+		return res.status(200).json({ message: 'User updated successfully', user: _.pick(user, ['email', 'name']) });
+	} catch(err){
+		console.log(err.message);
+		return res.status(500).json({ message: 'Something happened', error: err.message});
+	}
+})
+
+// Get user detail with id
+router.get('/:id', auth, async (req, res) => {
+	try {
+		console.log('In get user detail by ID');
+		const user = await User.findById(req.params.id);
+
+		if (!user) return res.status(404).json({ message: 'No user found' });
+
+		const userData = _.pick(user, ['email', 'name']);
 		return res.status(200).json({user : userData});
 	} catch(err) {
 		console.log(err.message);
 		return res.status(500).json({error: err.message});
 	}
 });
-
-// GET all users with pagination
-router.get('/', async (req, res) => {
-	try{
-		const page = parseInt(req.query.page) || 1;
-		const limit = parseInt(req.query.limit) || 5;
-		const skip = (page - 1) * limit;
-
-		const users = await User.find().skip(skip).limit(limit);
-
-		if(users.length === 0){
-			return res.status(200).json({ message: 'No Users found', users : []});
-		}
-
-		const usersData = users.map(user =>  _.pick(user, ['email', 'name']))
-		return res.status(200).json({users : usersData});
-	} catch(err) {
-		console.log(err.message);
-		return res.status(500).json({error: err.message});
-	}
-});
-
-// Register a user
-router.post('/register', async (req, res) => {
-	try{
-		const {error, value} = validate(req.body);
-		if(error) {
-			return res.status(400).json({ error: error.details[0].message });
-		}
-
-		const existingUser = await User.findOne({ email: value.email})
-		if(existingUser){
-			return res.status(400).json({ error: 'User with this email already exists' });
-		}
-
-		const salt = await bcrypt.genSalt(10);
-		const hashedPassword = await bcrypt.hash(value.password, salt);
-
-		const newUser = new User({
-			email: value.email,
-			password: hashedPassword,
-			name:{
-				firstName: value.name.firstName,
-				middleName: value.name.middleName,
-				lastName: value.name.lastName
-			},
-			address:{
-				  apartment: value.address.apartment,
-				  street: value.address.street,
-				  ward: value.address.ward,
-				  city: value.address.city,
-				  postalCode: value.address.postalCode
-			},
-			dob: new Date(value.dob),
-		})
-
-		await newUser.save();
-
-		return res.status(201).json({message: 'New user added', user: _.pick(newUser, ['email', 'name'])});
-	} catch(err){
-		console.log(err.message);
-		return res.status(500).json({error: err.message});
-	}
-})
-
-// // Update a user
-// router.put('/')
 
 module.exports = router
