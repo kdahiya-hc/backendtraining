1dbae1e8585af1ba3afe1df325ccf19827f7e0af
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-27 10:27:56 +0900

    FriendRequest Model added

---
+++ backendProject/models/FriendRequest.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const friendRequestSchema = new mongoose.Schema({
+	sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+	receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+	message: { type: String, default: 'Can you be my friend?' },
+	status: { type: String, enum: ['pending', 'declined', 'accepted'], default: 'pending' },
+	},{
+		timestamps: true,
+	});
+
+const FriendRequest = mongoose.model('FriendRequest', friendRequestSchema);
+
+function validateFriendRequest(data){
+	const schema = Joi.object({
+		sender: Joi.objectId().required(),
+		receiver: Joi.objectId().required(),
+		message: Joi.string().optional(),
+		status: Joi.string().valid('pending', 'declined', 'accepted').optional(),
+	}).options({ stripUnknown: true });
+
+	return schema.validate(data);
+}
+
+module.exports = { FriendRequest, validateFriendRequest };
