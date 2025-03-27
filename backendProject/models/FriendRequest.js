60189ec0b8d6aaa719e0699d08cd215324b73f0f
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-27 16:11:46 +0900

    FriendRequest Model updated

--- backendProject/models/FriendRequest.js
+++ backendProject/models/FriendRequest.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 const friendRequestSchema = new mongoose.Schema({
-	senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-	receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 	message: { type: String, default: 'Can you be my friend?' },
 	status: { type: String, enum: ['pending', 'declined', 'accepted'], default: 'pending' },
+	senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+	receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 	},{
 		timestamps: true,
 	});
@@ -14,10 +14,10 @@ const FriendRequest = mongoose.model('FriendRequest', friendRequestSchema);
 
 function validateFriendRequest(data){
 	const schema = Joi.object({
-		senderId: Joi.objectId().optional(),
-		receiverId: Joi.objectId().required(),
 		message: Joi.string().optional(),
 		status: Joi.string().valid('pending', 'declined', 'accepted').optional(),
+		senderId: Joi.objectId().optional(),
+		receiverId: Joi.objectId().required(),
 	}).options({ stripUnknown: true });
 
 	return schema.validate(data);
