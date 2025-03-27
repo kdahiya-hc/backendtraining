3b302b6fe5f1f5463a6bb7041faed4477ece4b25
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-27 16:11:37 +0900

    Comment model updated

--- backendProject/models/Comment.js
+++ backendProject/models/Comment.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 const commentSchema = new mongoose.Schema({
-	postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
-	userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 	content: { type: String, required: true },
+	postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+	commentedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 	},{ timestamps: true });
 
 const Comment = mongoose.model('Comment', commentSchema);
 
 function validateComment(data){
 	const schema = Joi.object({
-		postId: Joi.objectId().required(),
-		userId: Joi.objectId().optional(),
 		content: Joi.string().required(),
+		postId: Joi.objectId().required(),
+		commentedBy: Joi.objectId().optional(),
 	}).options({ stripUnknown: true });
 
 	return schema.validate(data);
