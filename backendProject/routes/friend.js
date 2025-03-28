0a4ebe6bbba5a1337a308a2d4b7565dddff0c2e9
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-28 10:02:10 +0900

    Remove friend endpoint added

--- backendProject/routes/friend.js
+++ backendProject/routes/friend.js
@@ -14,7 +14,7 @@ router.get('/', auth, async (req, res) => {
 
 		const user = await User.findById(req.user._id).populate({
 			path: 'friendsId',
-			select: 'email name'
+			select: '_id email name'
 		});
 
 		if (!user) {
@@ -23,7 +23,7 @@ router.get('/', auth, async (req, res) => {
 				message: 'No user found',
 				value: { user : { } }
 			});
-		};
+		}
 
 		const totalFriends = user.friendsId.length;
 		const friends = user.friendsId.slice(skip, skip + limit);
@@ -31,12 +31,12 @@ router.get('/', auth, async (req, res) => {
 		if (friends.length === 0){
 			return res.status(200).json({
 				success: false,
-				message: 'Nothing to show on this page, try removing all the limits!',
+				message: 'You either have no friends of too high expectation. Add some friends or try removing all the limits!',
 				value : { friends, totalFriends }
 			});
 		}
 
-		const friendsData = friends.map(friend => _.pick(friend, ['email', 'name']))
+		const friendsData = friends.map(friend => _.pick(friend, ['_id', 'email', 'name']))
 
 		return res.status(200).json({
 			success: true,
@@ -51,5 +51,64 @@ router.get('/', auth, async (req, res) => {
 		});
 	}
 });
+
+router.delete('/remove/:friendId', auth, async (req, res) => {
+	try {
+		console.log('In Remove a friend');
+		const user = await User.findById(req.user._id);
+
+		if (!user) {
+			return res.status(404).json({
+				success: false,
+				message: 'No user found',
+				value: { user : { } }
+			});
+		}
+
+		if (user._id.equals(req.params.friendId)){
+			return res.status(400).json({
+				success: false,
+				message: 'Expected, that you do not want to be friends with yourself',
+				value: {}
+			});
+		}
+
+		if (!user.friendsId.toString().includes(req.params.friendId)){
+			return res.status(400).json({
+				success: false,
+				message: 'No friends with this Id',
+				value: {}
+			});
+		}
+
+		const removedFriend = await User.findById(req.params.friendId).select('_id name email');
+
+		// For the req.params.friendId removing req.user._id
+		await User.findByIdAndUpdate(
+			req.params.friendId,
+			{ $pull: { friendsId: req.user._id } },
+			{ new: true }
+		);
+
+		// For the req.user._id removing the req.params.friendId
+		await User.findByIdAndUpdate(
+			req.user._id,
+			{ $pull: { friendsId: req.params.friendId} },
+			{ new: true }
+		);
+
+		return res.status(200).json({
+			success: true,
+			message: 'Friend has be removed successfully!',
+			value: { Removed : removedFriend }
+		});
+	} catch(err){
+		return res.status(500).json({
+			success: false,
+			message: err.message,
+			value: { }
+		});
+	}
+})
 
 module.exports = router;
