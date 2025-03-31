568c2e2e5c7d1621ca83d778af4693568801b1da
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-03-31 15:15:39 +0900

    updated friend.js route

--- backendProject/routes/friend.js
+++ backendProject/routes/friend.js
@@ -5,54 +5,7 @@ const auth = require('../middlewares/auth');
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 
-// Get all friends details with pagination
-router.get('/', auth, async (req, res) => {
-	try {
-		console.log('In get all friends with pagination');
-		const page = parseInt(req.query.page) || 1;
-		const limit = parseInt(req.query.limit) || 5;
-		const skip = (page - 1) * limit;
-
-		const user = await User.findById(req.user._id).populate({
-			path: 'friendsId',
-			select: '_id email name'
-		});
-
-		if (!user) {
-			return res.status(404).json({
-				success: false,
-				message: 'No user found',
-				value: { user : { } }
-			});
-		}
-
-		const totalFriends = user.friendsId.length;
-		const friends = user.friendsId.slice(skip, skip + limit);
-
-		if (friends.length === 0){
-			return res.status(200).json({
-				success: false,
-				message: 'You either have no friends of too high expectation. Add some friends or try removing all the limits!',
-				value : { friends, totalFriends }
-			});
-		}
-
-		const friendsData = friends.map(friend => _.pick(friend, ['_id', 'email', 'name']))
-
-		return res.status(200).json({
-			success: true,
-			message: 'Request successful',
-			value: { friends : friendsData, totalFriends }
-		});
-	} catch(err) {
-		return res.status(500).json({
-			success: false,
-			message: err.message,
-			value: { }
-		});
-	}
-});
-
+// remove friend
 router.delete('/remove/:friendId', auth, async (req, res) => {
 	try {
 		console.log('In Remove a friend');
@@ -111,5 +64,53 @@ router.delete('/remove/:friendId', auth, async (req, res) => {
 		});
 	}
 })
+
+// Get all friends details with pagination
+router.get('/', auth, async (req, res) => {
+	try {
+		console.log('In get all friends with pagination');
+		const page = parseInt(req.query.page) || 1;
+		const limit = parseInt(req.query.limit) || 5;
+		const skip = (page - 1) * limit;
+
+		const user = await User.findById(req.user._id).populate({
+			path: 'friendsId',
+			select: '_id email name'
+		});
+
+		if (!user) {
+			return res.status(404).json({
+				success: false,
+				message: 'No user found',
+				value: { user : { } }
+			});
+		}
+
+		const totalFriends = user.friendsId.length;
+		const friends = user.friendsId.slice(skip, skip + limit);
+
+		if (friends.length === 0){
+			return res.status(200).json({
+				success: false,
+				message: 'You either have no friends of too high expectation. Add some friends or try removing all the limits!',
+				value : { friends, totalFriends }
+			});
+		}
+
+		const friendsData = friends.map(friend => _.pick(friend, ['_id', 'email', 'name']))
+
+		return res.status(200).json({
+			success: true,
+			message: 'Request successful',
+			value: { friends : friendsData, totalFriends }
+		});
+	} catch(err) {
+		return res.status(500).json({
+			success: false,
+			message: err.message,
+			value: { }
+		});
+	}
+});
 
 module.exports = router;
