68235468cf1360b2b32317577f07649a820a168b
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-01 15:07:33 +0900

    updated the home.js

--- backendProject/routes/home.js
+++ backendProject/routes/home.js
@@ -1,9 +1,40 @@
 // /routes/home.js
 const express = require('express')
 const router = express.Router({ mergeParams: true });
+const crypto = require('crypto');
 
-router.get('/', (req, res) => {
-	res.status(200).json({ message: 'Hello'});
+router.get('/:number', (req, res) => {
+	try{
+		console.log('In / to check server is listening on port');
+		const number = parseInt(req.params.number);
+		if (Number.isInteger(number)) {
+			if (number%2 === 0){
+				return res.status(200).json({
+					success: true,
+					message: 'Number is even',
+					value: { number }
+				});
+			} else {
+				return res.status(200).json({
+					success: true,
+					message: 'Number is odd',
+					value: { number }
+				})
+			}
+		}
+
+		return res.status(400).json({
+			success: false,
+			message: 'Pass a number',
+			value: { Text: req.params.number }
+		})
+	} catch(err) {
+		return res.status(500).json({
+			success: false,
+			message: err.message,
+			value: { }
+		});
+	}
 });
 
 module.exports = router;
