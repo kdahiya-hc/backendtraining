d0907dbd81695968107fa286707a4f7e29645300
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-01 17:14:04 +0900

    implemented swagger documentation via JsDoc multiline comments in code for /{number} endpoint

--- backendProject/routes/home.js
+++ backendProject/routes/home.js
@@ -1,12 +1,104 @@
 // /routes/home.js
 const express = require('express')
 const router = express.Router({ mergeParams: true });
-const crypto = require('crypto');
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *   defaultResponse:
+ *    type: object
+ *    properties:
+ *     success:
+ *      type: boolean
+ *     message:
+ *      type: string
+ *     value:
+ *      type: object
+ *    example:
+ *     success: true
+ *     message: Passed
+ *     value: { key: value }
+ */
+
+/**
+ * @swagger
+ * /{number}:
+ *  get:
+ *   tags:
+ *    - /:number
+ *   summary: Check if a number is odd or even
+ *   description: |
+ *    Given a number in the URL path, the endpoint checks if the number is odd or even.
+ *
+ *    If no number is passed, or if the value is not a number, it returns a 400 error.
+ *   parameters:
+ *    - in: path
+ *      name: number
+ *      required: true
+ *      description: The number to check
+ *      schema:
+ *       example: 5
+ *   responses:
+ *    200:
+ *     description: Successfully received the number and checked if it is odd or even
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: "#/components/schemas/defaultResponse"
+ *        properties:
+ *         success:
+ *          type: boolean
+ *          example: true
+ *         message:
+ *          type: string
+ *          example: Number is even
+ *         value:
+ *          type: object
+ *          properties:
+ *           number:
+ *            type: integer
+ *            example: 0
+ *    400:
+ *     description: Bad Request - The input was not a valid number
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: "#/components/schemas/defaultResponse"
+ *        properties:
+ *         success:
+ *          type: boolean
+ *          example: false
+ *         message:
+ *          type: string
+ *          example: Pass a number
+ *         value:
+ *          type: object
+ *          properties:
+ *           number:
+ *            type: string
+ *            example: hello
+ *    500:
+ *     description: Internal Server Error
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: "#/components/schemas/defaultResponse"
+ *        properties:
+ *         success:
+ *          type: boolean
+ *          example: false
+ *         message:
+ *          type: string
+ *          example: Internal server error
+ *         value:
+ *          type: object
+ *          properties: {}
+*/
 
 router.get('/:number', (req, res) => {
 	try{
 		console.log('In / to check server is listening on port');
-		const number = parseInt(req.params.number);
+		const number = Number(req.params.number);
 		if (Number.isInteger(number)) {
 			if (number%2 === 0){
 				return res.status(200).json({
--- backendProject/server.js
+++ backendProject/server.js
@@ -6,43 +6,34 @@ Joi.objectId = require('joi-objectid')(Joi);
 const morgan = require('morgan');
 const express = require('express');
 const mongoose = require('mongoose');
+// You can either define the Swagger documentation inline using JSDoc comments,
+// or load the documentation from an external JSON or YAML file. Here it is swagger.yaml file
 const swaggerUI = require('swagger-ui-express');
-// Either do the swagger documentation in code and load or
-// do the swagger documentation in a json or yaml file and load it.
 const swaggerJsDoc = require('swagger-jsdoc');
-// const YAML = require('yamljs');
-// const swaggerDocument = YAML.load('./swagger.yaml');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
-// database configuration
-const dbConfig = config.get('db');
-const database = process.env[dbConfig.database];
-
-const dbUri = `mongodb://${dbConfig.user}:${dbConfig.pass}@${dbConfig.host}:${dbConfig.port}/${database}?authSource=admin`;
-
-mongoose.connect(dbUri)
-	.then(() => console.log(`Connected to ${database}`))
-	.catch(err => console.log(err));
-
-// swagger configuration
+// Swagger configuration: Set up Swagger UI with OpenAPI specs for API documentation
 const options = {
-		definition: {
-			openapi: '3.0.0',
-			info: {
-				title: 'Social Media API',
-				description: 'This is the social media api created based on the backend training project document.\n\nSome useful links: \n\n - [The Backend training repository](https://github.com/swagger-api/swagger-petstore)',
-				version: '1.0.0',
-			},
-			servers: [
-				{
-					url: 'http://localhost:8000',
-					description: 'Development environment'
-				},
-			],
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'Social Media API',
+			version: '1.0.0',
+			description: 'This is the social media api created based on the backend training project document.\n\nSome useful links: \n\n - [The Backend training repository(public)](https://github.com/kdahiya-hc/backendtraining)',
 		},
-		apis: [ './routes/*.js', './models/*.js']
-		// Read API docs from route and model files
-	}
+		servers: [
+			{
+				url: 'http://localhost:8000',
+				description: 'Development environment'
+			},
+		],
+	},
+	// This below line tells where to read the paths or components from. Read API docs from route and model files
+	apis: [ './routes/*.js', './models/*.js']
+}
 
+// Initialize swaggerJSDoc, specs is used to build the ui
 const specs = swaggerJsDoc(options);
 
 // express application initialization
@@ -51,10 +42,11 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan(process.env.NODE_ENV));
+app.use(morgan('dev'));
 
-// serve the swagger ui
+// Serving the Swagger UI at the '/api-docs' route to visualize the API documentation
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/', require('./routes/home'));
@@ -65,6 +57,16 @@ app.use('/api/friends', require('./routes/friend'));
 app.use('/api/posts', require('./routes/post'));
 app.use('/api/likes/:postId', require('./routes/like'));
 app.use('/api/comments', require('./routes/comment'));
+
+// Database configuration
+const dbConfig = config.get('db');
+const database = process.env[dbConfig.database];
+
+const dbUri = `mongodb://${dbConfig.user}:${dbConfig.pass}@${dbConfig.host}:${dbConfig.port}/${database}?authSource=admin`;
+
+mongoose.connect(dbUri)
+	.then(() => console.log(`Connected to ${database}`))
+	.catch(err => console.log(err));
 
 // server configuration
 const PORT = config.get('port');
