f84449ac17c009880971b6a61d01eda1b53f2d95
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-02 15:04:55 +0900

    comment schema added to component

--- backendProject/models/Comment.js
+++ backendProject/models/Comment.js
@@ -2,6 +2,32 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     comment:
+ *       description: Comment under a post
+ *       type: object
+ *       properties:
+ *         content:
+ *           type: string
+ *           description: The content of the comment
+ *         postId:
+ *           type: string
+ *           description: 24 hex decimals or 12 bytes id of post
+ *         commentedBy:
+ *           type: string
+ *           description: 24 hex decimals or 12 bytes id of user
+ *       required:
+ *         - content
+ *       additionalProperties: false
+ *       example:
+ *         content: Hey that is my picture, give credits!
+ *         postId: aAbB1234cCdD5678
+ *         commentedBy: aAbB1234cCdD5678
+*/
+
 const commentSchema = new mongoose.Schema({
 	content: { type: String, required: true },
 	postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
