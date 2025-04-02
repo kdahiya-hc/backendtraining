ea9e5823b874cbcb6116f7ca36e836dcd2f76c2d
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-02 15:06:13 +0900

    friendRequest schema added to components

--- backendProject/models/FriendRequest.js
+++ backendProject/models/FriendRequest.js
@@ -2,6 +2,42 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     friendRequest:
+ *       description: A friend request
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           description: The message along with the request
+ *           default: Can you be my friend?
+ *         status:
+ *           type: string
+ *           description: The status of the request
+ *           default: pending
+ *           enum:
+ *             - pending
+ *             - declined
+ *             - accepted
+ *         from:
+ *           type: string
+ *           description: 24 hex decimals or 12 bytes id of sender
+ *         to:
+ *           type: string
+ *           description: 24 hex decimals or 12 bytes id of receiver
+ *       required:
+ *         - to
+ *       additionalProperties: false
+ *       example:
+ *         message: Can you be my friend?
+ *         status: pending
+ *         from: aAbB1234cCdD5678
+ *         to: aAbB1234cCdD5678
+ */
+
 const friendRequestSchema = new mongoose.Schema({
 	message: { type: String, default: 'Can you be my friend?' },
 	status: { type: String, enum: ['pending', 'declined', 'accepted'], default: 'pending' },
