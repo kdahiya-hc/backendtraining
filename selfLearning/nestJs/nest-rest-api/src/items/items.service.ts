04782f589408071aebe97446965cc8e05ba9a2b9
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-08 16:30:06 +0900

    updated items.service.ts

--- selfLearning/nestJs/nest-rest-api/src/items/items.service.ts
+++ selfLearning/nestJs/nest-rest-api/src/items/items.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/items.interfaces';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+	private readonly items: Item[] = [
+		{
+			id: '1',
+			name: 'Table',
+			description: 'Something to place on',
+			qty: 100
+		},
+		{
+			id: '2',
+			name: 'Chair',
+			description: 'Something to sit on',
+			qty: 100
+		},
+		{
+			id: '3',
+			name: 'Desk',
+			description: 'Something to keep on',
+			qty: 100
+		},
+	];
+
+	findAll(): Item[] {
+		return this.items;
+	}
+}
