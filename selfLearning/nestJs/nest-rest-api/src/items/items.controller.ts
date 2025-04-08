46ef1393bcc5b500ae6e512527adb2080bc43255
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-08 15:43:20 +0900

    nestJsCrashCourse CRUD

--- selfLearning/nestJs/nest-rest-api/src/items/items.controller.ts
+++ selfLearning/nestJs/nest-rest-api/src/items/items.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Body, Get, Put, Delete, Post, Req, Res } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+import { UpdateItemDto } from './dto/update-item.dto';
+import { Request, Response } from 'express';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+	@Get()
+	findAll(@Req() req: Request, @Res() res: Response): Response {
+		console.log(req.url);
+		return res.json({'message': 'This is a message'});
+	}
+
+	@Get('/:id')
+	findOne(@Param('id') id: string): string {
+		return `Item ${id}`;
+	}
+
+	@Post()
+	create(@Body() createItemDto: CreateItemDto): string {
+	return `Name: ${createItemDto.name}\nDescription: ${createItemDto.description}`
+	}
+
+	@Delete('/:id')
+	deleteOne(@Param('id') id: string): string {
+		return `Delete ${id}`;
+	}
+
+	@Put('/:id')
+	updateOne(@Param('id') id: string, @Body() updateItemDto: UpdateItemDto): string {
+		return `Update ${id} - Name: ${updateItemDto.name}`;
+	}
+}
