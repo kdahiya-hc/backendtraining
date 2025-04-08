fe88997a3befe8c2d591afbb734c28ae9ae4562c
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-08 16:32:20 +0900

    dependency injected and Get added for findAll

--- selfLearning/nestJs/nest-rest-api/src/items/items.controller.ts
+++ selfLearning/nestJs/nest-rest-api/src/items/items.controller.ts
@@ -2,13 +2,17 @@ import { Controller, Param, Body, Get, Put, Delete, Post, Req, Res } from '@nest
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
 import { Request, Response } from 'express';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/items.interfaces';
 
 @Controller('items')
 export class ItemsController {
+	// dependency being injected
+	constructor( private readonly itemsService: ItemsService) {}
+
 	@Get()
-	findAll(@Req() req: Request, @Res() res: Response): Response {
-		console.log(req.url);
-		return res.json({'message': 'This is a message'});
+	findAll(): Item[] {
+		return this.itemsService.findAll();
 	}
 
 	@Get('/:id')
