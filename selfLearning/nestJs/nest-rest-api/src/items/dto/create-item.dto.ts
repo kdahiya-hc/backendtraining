08403bf973afa7b19086a1c36a7f5b3873213386
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-09 09:15:42 +0900

    similar changes to dto with class validator

--- selfLearning/nestJs/nest-rest-api/src/items/dto/create-item.dto.ts
+++ selfLearning/nestJs/nest-rest-api/src/items/dto/create-item.dto.ts
@@ -1,5 +1,14 @@
+import { IsNumber, IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CreateItemDto {
+	@IsString()
+	@IsNotEmpty()
 	readonly name: string;
+	@IsString()
+	@IsNotEmpty()
 	readonly description: string;
+	@Type(() => Number)
+	@IsNumber()
 	readonly qty: number;
 }
