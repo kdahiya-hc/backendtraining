10e5e7ff530c8cb82880aba01ebc725a66696ff8
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-10 13:26:56 +0900

    FCC-35 #feat added methods to service, routes to controller

--- selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/song/song.controller.ts
+++ selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/song/song.controller.ts
@@ -1,5 +1,33 @@
-import { Controller} from "@nestjs/common";
+import { CreateSongDto } from "./dto/create-song.dto";
+import { SongService } from "./song.service";
+import { Controller, Body, Get, Post, Patch, Delete } from "@nestjs/common";
 
 @Controller('songs')
 export class SongController {
+	constructor (private readonly songService: SongService) {}
+
+	@Get()
+	findAll(): string {
+		return this.songService.findAll();
+	}
+
+	@Get()
+	findOne(): string {
+		return this.songService.findOne();
+	}
+
+	@Post()
+	create(@Body() createSong : CreateSongDto): any {
+		return this.songService.create();
+	}
+
+	@Patch()
+	update(): string {
+		return this.songService.update();
+	}
+
+	@Delete()
+	delete(): string {
+		return this.songService.delete();
+	}
 }
--- selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/song/song.service.ts
+++ selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/song/song.service.ts
@@ -2,4 +2,23 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class SongService{
+	findAll(): string {
+		return 'find all song endpoint'
+	}
+
+	findOne(): string {
+		return 'find one song endpoint'
+	}
+
+	create(): string {
+		return 'create all song endpoint'
+	}
+
+	update(): string {
+		return 'update all song endpoint'
+	}
+
+	delete(): string {
+		return 'delete all song endpoint'
+	}
 }
