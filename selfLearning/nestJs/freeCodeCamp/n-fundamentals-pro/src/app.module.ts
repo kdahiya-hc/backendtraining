0c00e67782435f9d12acbad366ecf206e6163ab0
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-10 13:53:21 +0900

    FCC-41 #Fix full URL for all routes logged

--- selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/app.module.ts
+++ selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongModule } from './song/song.module';
@@ -11,6 +11,7 @@ import { LoggerMiddleware } from './middlewares/logger.middleware';
 })
 export class AppModule implements NestModule{
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('songs');
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+    // consumer.apply(LoggerMiddleware).forRoutes({ path: 'songs', method: RequestMethod.PATCH });
   }
 }
--- selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/middlewares/logger.middleware.ts
+++ selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/middlewares/logger.middleware.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express";
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
 	use(req: Request, res: Response, next: NextFunction): void {
-		console.log(`${req.method} ${req.url} ${new Date().toDateString()}`);
+		console.log(`${req.method} ${req.originalUrl} ${new Date().toDateString()}`);
 		next();
 	}
 }
