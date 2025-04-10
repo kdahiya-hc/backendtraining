ac63bee1911168bf27ab15ab7f42668da916b88d
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-10 14:42:15 +0900

    FCC-51 #Transform-Pipes ParseIntPipe for Param

--- selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/song/song.controller.ts
+++ selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/song/song.controller.ts
@@ -1,18 +1,18 @@
 import { CreateSongDto } from "./dto/create-song.dto";
 import { SongService } from "./song.service";
-import { Controller, Body, Get, Post, Patch, Delete, Param } from "@nestjs/common";
+import { Controller, Body, Get, Post, Patch, ParseIntPipe, Delete, HttpStatus, Param } from "@nestjs/common";
 
 @Controller('songs')
 export class SongController {
 	constructor (private readonly songService: SongService) {}
 
 	@Get()
-	findAll(): string {
+	findAll(){
 		return this.songService.findAll();
 	}
 
 	@Get('/:id')
-	findOne(@Param('id') id: number): string {
+	findOne(@Param('id', new ParseIntPipe()) id: number): string {
 		return this.songService.findOne();
 	}
 
