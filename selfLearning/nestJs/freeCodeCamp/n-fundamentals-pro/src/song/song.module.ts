a61af14cd6ec96c94c64e0b5bb8afc602b29764d
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-10 16:14:02 +0900

    FCC-61 #feat Custom provider: by useValue

--- selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/song/song.module.ts
+++ selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/song/song.module.ts
@@ -1,12 +1,36 @@
 import { Module } from '@nestjs/common';
 import { SongController } from './song.controller';
 import { SongService } from './song.service';
+const mockingSongService = {
+	findAll() {
+		return 'find all song endpoint';
+	},
+
+	findOne(id) {
+		return `find one song endpoint ${id}`
+	},
+
+	create(createSongData) {
+		return `create a song endpoint ${JSON.stringify(createSongData)}`
+	},
+
+	update(updateSongData) {
+		return `update a song endpoint ${JSON.stringify(updateSongData)}`
+	},
+
+	delete(id) {
+		return `delete a song endpoint ${id}`
+	},
+}
 
 @Module({
 	imports: [],
 	controllers: [SongController],
+	// providers: [
+	// 	{ provide: SongService, useClass: SongService }
+	// ],
 	providers: [
-		{ provide: SongService, useClass: SongService }
-	],
+		{ provide: SongService, useValue: mockingSongService }
+	]
 })
 export class SongModule {}
