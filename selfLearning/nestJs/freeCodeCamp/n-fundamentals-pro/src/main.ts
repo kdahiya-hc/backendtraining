865a087823776878743ce1835e16f6948c1be668
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-11 13:19:59 +0900

    #fix Global pipelines added

--- selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/main.ts
+++ selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/main.ts
@@ -5,9 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-      // whitelist: true,
-      // transform: true,
-      // forbidNonWhitelisted: true,
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      disableErrorMessages: false, // important to switch in production
     }
   ));
   await app.listen(process.env.PORT ?? 3000);
