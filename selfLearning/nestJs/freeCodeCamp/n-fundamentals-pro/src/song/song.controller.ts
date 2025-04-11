555e63ee131a0fe7198a6e32dd388b2ec58edb9c
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-11 13:21:04 +0900

    #fix update controller with dtos

--- selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/song/song.controller.ts
+++ selfLearning/nestJs/freeCodeCamp/n-fundamentals-pro/src/song/song.controller.ts
@@ -1,34 +1,40 @@
 import { CreateSongDTO } from "./dto/create-song.dto";
+import { ResponseSongDTO } from "./dto/response-song.dto";
 import { UpdateSongDTO } from "./dto/update-song.dto";
 import { SongService } from "./song.service";
-import { Controller, Body, Get, Post, Patch, ParseIntPipe, Delete, HttpStatus, Param } from "@nestjs/common";
+import { Controller, UsePipes, ValidationPipe, Body, Get, Post, Patch, ParseIntPipe, Delete, Param } from "@nestjs/common";
 
 @Controller('songs')
 export class SongController {
 	constructor (private readonly songService: SongService) {}
 
+	@Get('service-info')
+	getServiceInfo(): { instanceId: string } {
+		return this.songService.getServiceInfo();
+	}
+
 	@Get()
-	findAll(){
+	findAll(): ResponseSongDTO[] {
 		return this.songService.findAll();
 	}
 
 	@Get('/:id')
-	findOne(@Param('id', ParseIntPipe) id: number) {
+	findOne(@Param('id', ParseIntPipe) id: number): ResponseSongDTO {
 		return this.songService.findOne(id);
 	}
 
 	@Post()
-	create(@Body() createSongDto: CreateSongDTO) {
+	create(@Body() createSongDto: CreateSongDTO): ResponseSongDTO {
 		return this.songService.create(createSongDto);
 	}
 
 	@Patch('/:id')
-	update(@Param('id', ParseIntPipe) id: number, @Body() updateSongDto: UpdateSongDTO) {
+	update(@Param('id', ParseIntPipe) id: number, @Body() updateSongDto: UpdateSongDTO): ResponseSongDTO {
 		return this.songService.update(id, updateSongDto);
 	}
 
 	@Delete('/:id')
-	delete(@Param('id', ParseIntPipe) id: number) {
+	delete(@Param('id', ParseIntPipe) id: number): ResponseSongDTO {
 		return this.songService.delete(id);
 	}
 }
