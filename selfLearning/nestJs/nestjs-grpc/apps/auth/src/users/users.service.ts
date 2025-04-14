533ba87758449892dc08ca569144f58273b10c8d
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-14 16:11:21 +0900

    gRPCMS-41 #feat update to properly implement CRUD using gRPC methods *Remove unused imports *Dto coming from @app/common *implement OnModuleInit to create users on initial run. it is invoked by bootstrap hook *For queryUsers we have to return Observable<Users>  - takes in paginationDtoStream: Observable<PaginationDto> which returns strea of Observable<Users>  - We create subject that is reference to the stream  - We attach functionalities to subject when caller calls the stream and it when completes  - Subject is sub section of Users as it will chunk the data Subject<Users>, we stream to client the data in chunks  - Set onNext and onComplete  - onNext : happens when caller request more data. Finds next set of users by taking in start and skip  - onComplete = when caller is done with data we can close connection to db etc here or close async functions  subject.complete()  - now we subscribe to methods  - return the subject back to caller as subject.Observable();

--- selfLearning/nestJs/nestjs-grpc/apps/auth/src/users/users.service.ts
+++ selfLearning/nestJs/nestjs-grpc/apps/auth/src/users/users.service.ts
@@ -1,26 +1,87 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { User, Users, CreateUserDto, UpdateUserDto, PaginationDto } from '@app/common';
+import { randomUUID } from 'crypto';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+export class UsersService implements OnModuleInit {
+  private readonly users: User[] = [];
+
+  onModuleInit() {
+    for (let i = 0; i<= 100; i++) {
+      this.create({
+        username: randomUUID(),
+        password: randomUUID(),
+        age: 0
+      });
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  create(createUserDto: CreateUserDto): User {
+    const user: User = {
+      ...createUserDto,
+      subscribed: false,
+      socialMedia: {},
+      id: randomUUID()
+    }
+
+    this.users.push(user);
+    return user;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findAll(): Users {
+    return { users: this.users };
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  findOne(id: string): User {
+    const user = this.users.find((user) => user.id === id)
+    if (!user) throw new NotFoundException(`User with ${id} not found`);
+    return user;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto): User {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex !== -1) {
+      this.users[userIndex] = {
+        ...this.users[userIndex],
+        ...updateUserDto,
+      };
+      return this.users[userIndex];
+    }
+
+    throw new NotFoundException(`User with ${id} not found`);
+  }
+
+  remove(id: string): User {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex !== -1) {
+      return this.users.splice(userIndex)[0];
+    }
+
+    throw new NotFoundException(`User with ${id} not found`);
+  }
+  
+/* `paginationDtoStream` in the `queryUsers` method is an Observable stream that emits
+  `PaginationDto` objects. The method subscribes to this stream and processes the emitted
+  `PaginationDto` objects to return a paginated list of users. It calculates the start
+  index based on the page number and skip value provided in the `PaginationDto`, then
+  returns a subset of users from the internal `users` array based on the calculated start
+  index and skip value. */
+  queryUsers(paginationDtoStream: Observable<PaginationDto>): Observable<Users> {
+    const subject = new Subject<Users>();
+
+    const onNext = (paginationDto : PaginationDto) => {
+      const start = paginationDto.page * paginationDto.skip;
+      subject.next({
+        users: this.users.splice(start, start+paginationDto.skip),
+      });
+    };
+    const onComplete = () => subject.complete();
+
+    paginationDtoStream.subscribe({
+      next: onNext,
+      complete: onComplete,
+    });
+    return subject.asObservable();
   }
 }
