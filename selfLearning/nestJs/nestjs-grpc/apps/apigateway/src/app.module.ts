d7d60a50ff15352b5dde65a70ac53d384c1af365
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-04-15 15:20:25 +0900

    gRPCMS #fix rate limitting

--- selfLearning/nestJs/nestjs-grpc/apps/apigateway/src/app.module.ts
+++ selfLearning/nestJs/nestjs-grpc/apps/apigateway/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          name: 'short',
+          ttl: 1000,
+          limit: 3,
+        },
+        {
+          name: 'long',
+          ttl: 60000,
+          limit: 100,
+        },
+      ],
+    })
+    ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
