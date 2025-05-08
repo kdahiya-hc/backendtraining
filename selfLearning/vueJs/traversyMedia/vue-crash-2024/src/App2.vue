a0c05dd370b3b31e9093b50bcea60ed713f69e89
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-05-08 18:54:21 +0900

    TMvcc-33 #feat: v-on:click and @click, options api end

---
+++ selfLearning/vueJs/traversyMedia/vue-crash-2024/src/App2.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      status: 'pending',
+      tasks : ['Task one', 'Task two', 'Task three']
+    };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if (this.status === 'pending') {
+        this.status = 'inactive';
+      } else {
+        this.status = 'active';
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <p v-if="status === 'active'">User {{ name }} is active</p>
+  <p v-else-if="status === 'pending'">User {{ name }} is pending</p>
+  <p v-else>User {{ name }} is inactive</p>
+  <button @click="toggleStatus">Toggle button with @click</button>
+  <button v-on:click="toggleStatus">Toggle button with v-on:click</button>
+  <h3>--Tasks--</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <a v-bind:href="'https://www.google.com'">Click for Google</a>
+  <a :href="'https://www.google.com'">Click for Google</a>
+
+</template>
+
+<style scoped>
+h1 {
+  color: aquamarine;
+}
+.app {
+  background-color: white;
+  color: black;
+  min-height: 100vh;
+  padding: 2rem;
+}
+</style>
