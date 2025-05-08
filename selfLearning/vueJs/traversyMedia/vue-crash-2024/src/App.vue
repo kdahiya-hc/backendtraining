15bcfe75c7c61a7aa2e8a6ddc89620e76aee8a2e
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-05-08 19:20:06 +0900

    TMvcc-40 #feat: composition api

--- selfLearning/vueJs/traversyMedia/vue-crash-2024/src/App.vue
+++ selfLearning/vueJs/traversyMedia/vue-crash-2024/src/App.vue
@@ -1,43 +1,58 @@
 <script>
+import { ref } from 'vue';
+
 export default {
-  data() {
-    return {
-      name: 'John Doe',
-      status: 'pending',
-      tasks : ['Task one', 'Task two', 'Task three']
-    };
-  },
-  methods: {
-    toggleStatus() {
-      if (this.status === 'active') {
-        this.status = 'pending';
-      } else if (this.status === 'pending') {
-        this.status = 'inactive';
+  setup() {
+    const name = ref('John Doe');
+    const status = ref('active');
+    const tasks = ref(['Task One', 'Task Two', 'Task Three']);
+
+    const toggleStatus = () => {
+      if (status.value === 'active') {
+        status.value = 'pending';
+      } else if (status.value === 'pending') {
+        status.value = 'inactive';
       } else {
-        this.status = 'active';
+        status.value = 'active';
       }
+    };
+
+    return {
+      name,
+      toggleStatus,
+      tasks,
+      status,
     }
-  }
-}
+  },
+};
 </script>
 
 <template>
   <p v-if="status === 'active'">User {{ name }} is active</p>
   <p v-else-if="status === 'pending'">User {{ name }} is pending</p>
   <p v-else>User {{ name }} is inactive</p>
-  <button @click="toggleStatus">Toggle Status</button>
+
+  <button @click="toggleStatus">Toggle button with @click</button>
+  <button v-on:click="toggleStatus">Toggle button with v-on:click</button>
 
   <h3>--Tasks--</h3>
   <ul>
     <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
-  <a v-bind:href="'https://www.google.com'">Click for Google</a>
-  <a :href="'https://www.google.com'">Click for Google</a>
+
+  <!-- <a v-bind:href="'https://www.google.com'">Click for Google</a> -->
+  <!-- <a :href="'https://www.google.com'">Click for Google</a> -->
 
 </template>
 
 <style scoped>
 h1 {
   color: aquamarine;
+}
+.app {
+  background-color: white;
+  color: black;
+  min-height: 100vh;
+  padding: 2rem;
 }
 </style>
