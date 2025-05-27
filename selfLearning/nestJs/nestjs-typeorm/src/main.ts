0445bd9ab9086346a268594664a1377809518b59
kdahiya-hc <kishan_dahiya@heartcore.co.jp>
2025-05-27 14:54:10 +0900

    #fix: configService

--- selfLearning/nestJs/nestjs-typeorm/src/main.ts
+++ selfLearning/nestJs/nestjs-typeorm/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') ?? 3000;
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,7 +15,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  await app.listen(port);
 
-  await app.listen(process.env.PORT ?? 3000);
+  console.log(`listening on http://127.0.0.1:${port}/}`);
 }
 void bootstrap();
